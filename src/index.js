e1fcb934091ad519edfc25274a78474f9e81c98f
priyanka yadav <py631588@gmail.com>
2020-12-23 20:50:31 +0530

    functionalcomponent_import_export

--- src/index.js
+++ src/index.js
@@ -4,35 +4,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//create functional component
-//1.Example of Named Export
-//import {L} from './components/L';
-//import {M} from './components/M';
-//import {N} form './components/N';
-//import {O} from './components/O';
-//import {P} from './components/P';
-//import {Q} from './components/Q';
-//import {R} from './components/R';
-//import {S} from './components/S';
+
 //2.Example of Deafault Export
 import A from './components/A';
 
-//import C from './components/C';
-//import D from './components/D';
-//import E from './components/E';
-//import F from './components/F';
-//import G from './components/G';
-//import H from './components/H';
-//import I from './components/I';
-//import J from './components/J';
-//import K from './components/K';
-//import T from './components/T';
-//import U from './components/U';
-//import V from './components/V';
-//import W from './components/W';
-//import X from './components/X';
-//import Y from './components/Y';
-//import Z from './components/Z';
+
 
 
 //object.method();
